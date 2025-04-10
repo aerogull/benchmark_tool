92932f16133add9de9a4ed9025a543ecd93e8ae4
myhauntedspoon <drewhinckley2009@gmail.com>
2025-04-10 15:32:31 -0500

    Whitespace owed me rent.

--- main.c
+++ main.c
@@ -2,38 +2,24 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-
 clock_t t;
-
 double time_taken;
 int passloop;
 double timetime;
-
 extern double m1[250][250];
 extern double m2[250][250];
 extern double c[250][250];
-
 int when_record(int indicator) {
-    if (indicator == 0) {
-        int answer;
-        printf("Would you like to record individual tests or the average?[0]Each/[1]Average/[2]None\n");
-        printf(">>");
-        scanf("%d", &answer);
-        if (answer == 0 || answer == 1 || answer == 2) {
-            return answer;
-        } else {
-            return 1;
-        }
-    } else {
-        return indicator;
-    }
+    if (indicator != 0) return indicator;
+    int answer;
+    printf("Would you like to record individual tests or the average?\n\n[0] - Each\n[1] - Average\n[2] - None\n\n>> ");
+    scanf("%d", &answer);
+    return (answer >= 0 && answer <= 2) ? answer : 1;
 }
-
 void reset() {
     timetime = 0;
     passloop = 0;
 }
-
 void clear(int indicator) {
     FILE *f = NULL;
     if (indicator == 0) {
@@ -52,33 +38,28 @@ void clear(int indicator) {
         if (f) fclose(f);
     }
 }
-
 double startc() {
     t = clock();
     return t;
 }
-
 double endc() {
     t = clock() - t;
     time_taken = ((double)t) / (double)CLOCKS_PER_SEC;
     timetime += (double)time_taken;
     return time_taken;
 }
-
 int run_times(int indicator) {
     if (indicator == 0) {
         int answer;
         printf("How many passes? (10 will take around 20-120 seconds depending on test)\n");
-        printf(">>");
+        printf(">> ");
         scanf("%d", &answer);
         return answer;
     } else {
         return indicator;
     }
 }
-
 void write_scores(int test) {
-    //opens and appends based on the test number
     if (passloop == 0 || test < 1 || test > 10) return;
     char filename[20];
     snprintf(filename, sizeof(filename), "scores%d.txt", test);
@@ -90,9 +71,7 @@ void write_scores(int test) {
     fprintf(f, "%.10f\n", timetime / passloop);
     fclose(f);
 }
-
 void write_all_scores(int test) {
-    // This is for writing the time of each test, and not the average
     if (test < 1 || test > 10) return;
     char filename[20];
     snprintf(filename, sizeof(filename), "scores%d.txt", test);
@@ -101,7 +80,6 @@ void write_all_scores(int test) {
     fprintf(f, "%.10f\n", time_taken);
     fclose(f);
 }
-
 void mult_mat(int not, int runs, int record){
     reset();
     int x,y;
@@ -131,7 +109,6 @@ void mult_mat(int not, int runs, int record){
         write_scores(1);
     }
 }
-
 void check_mat(int not, int runs, int record){
     reset();
     int x, y;
@@ -165,7 +142,6 @@ void check_mat(int not, int runs, int record){
         write_scores(2);
     }
 }
-
 void print_speed(int not, int runs, int record){
     reset();
     for (passloop = 0; passloop < runs; passloop++) {
@@ -184,7 +160,6 @@ void print_speed(int not, int runs, int record){
         write_scores(3);
     }
 }
-
 void string(long long int not, int runs, int record){
     reset();
     char string[2000000] = "c";
@@ -206,7 +181,6 @@ void string(long long int not, int runs, int record){
         write_scores(4);
     }
 }
-
 void pi(long long int not, int runs, int record){
     reset();
     long double p = 0;
@@ -230,14 +204,12 @@ void pi(long long int not, int runs, int record){
         write_scores(5);
     }
 }
-
 float fib(float num) {
     if (num <= 1){
         return num;
     }
     return fib(num-1) + fib(num-2);
 }
-
 void fib1(int not, int runs, int record){
     reset();
     for (passloop = 0; passloop < runs; passloop++) {
@@ -257,7 +229,6 @@ void fib1(int not, int runs, int record){
     }
     printf("%.10f\n", timetime / passloop);
 }
-
 void division(int not, int runs, int record){
     reset();
     int x, y;
@@ -287,7 +258,6 @@ void division(int not, int runs, int record){
         write_scores(7);
     }
 }
-
 void doubles(int not, int runs, int record){
     reset();
     long int number1 = 1;
@@ -308,7 +278,6 @@ void doubles(int not, int runs, int record){
         write_scores(8);
     }
 }
-
 void file_write(int not, int runs, int record){
     reset();
     FILE *test = fopen("test", "a");
@@ -340,7 +309,6 @@ void file_write(int not, int runs, int record){
         write_scores(9);
     }
 }
-
 int fib3(int not) {
     int n1 = 0, n2 = 1, n3, i;
     for (i = 0; i <= not; i++) {
@@ -350,7 +318,6 @@ int fib3(int not) {
     }
     return n2;
 }
-
 void fib4(int not, int runs, int record){
     reset();
     for (passloop = 0; passloop < runs; passloop++) {
@@ -370,14 +337,13 @@ void fib4(int not, int runs, int record){
     }
     printf("%.10f\n", timetime / passloop);
 }
-
 void queue(){
     int tests[50];
     int runs[50];
     int rec;
     int run = 0;
     while (1){
-        printf("Enter the test number you would like to run or 0 to finish:");
+        printf("Enter the test number you would like to run or 0 to finish: ");
         scanf("%d", &tests[run]);
         if (tests[run] <= 0 || tests[run] > 10){
             break;
@@ -387,45 +353,25 @@ void queue(){
     }
     rec = when_record(0);
     for (int i = 0; i < run; i++){
-        if (tests[i] == 1){
-            mult_mat(50000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 2){
-            check_mat(50000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 3){
-            print_speed(2000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 4){
-            string(300000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 5){
-            pi(1000000000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 6){
-            fib1(150, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 7){
-            division(70000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 8){
-            doubles(900000000, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 9){
-            file_write(100, run_times(runs[i]), rec);
-        }
-        else if (tests[i] == 10){
-            fib4(100000, run_times(runs[i]), rec);
+        switch(tests[i]) {
+            case 1: mult_mat(50000, run_times(runs[i]), rec); break;
+            case 2: check_mat(50000, run_times(runs[i]), rec); break;
+            case 3: print_speed(2000, run_times(runs[i]), rec); break;
+            case 4: string(300000, run_times(runs[i]), rec); break;
+            case 5: pi(1000000000, run_times(runs[i]), rec); break;
+            case 6: fib1(150, run_times(runs[i]), rec); break;
+            case 7: division(70000, run_times(runs[i]), rec); break;
+            case 8: doubles(900000000, run_times(runs[i]), rec); break;
+            case 9: file_write(100, run_times(runs[i]), rec); break;
+            case 10: fib4(100000, run_times(runs[i]), rec); break;
         }
     }
 }
-
 void enterToContinue() {
     printf("Press Enter to continue...");
     while (getchar() != '\n');
     getchar();
 }
-
 int main() {
     int ans;
     while (1) {
@@ -445,53 +391,24 @@ int main() {
         printf("                [12]- Clear logs.\n");
         printf("                [0]- Quit\n");
         printf("---------------------------------------------------------------\n");
-        printf(">>");
+        printf(">> ");
         scanf("%d", &ans);
         switch(ans) {
-            case 1:
-                mult_mat(50000, run_times(0), when_record(0));
-                break;
-            case 2:
-                check_mat(50000, run_times(0), when_record(0));
-                break;
-            case 3:
-                print_speed(2000, run_times(0), when_record(0));
-                break;
-            case 4:
-                string(300000, run_times(0), when_record(0));
-                break;
-            case 5:
-                pi(1000000000, run_times(0), when_record(0));
-                break;
-            case 6:
-                fib1(150, run_times(0), when_record(0));
-                break;
-            case 7:
-                division(70000, run_times(0), when_record(0));
-                break;
-            case 8:
-                doubles(900000000, run_times(0), when_record(0));
-                break;
-            case 9:
-                file_write(100, run_times(0), when_record(0));
-                break;
-            case 10:
-                fib4(100000, run_times(0), when_record(0));
-                break;
-            case 11:
-                queue();
-                break;
-            case 12:
-                printf("Enter the log number you would like to clear or 0 to clear all: ");
-                int log;
-                scanf("%d", &log);
-                clear(log);
-                break;
-            case 0:
-                return 0;
-            default:
-                printf("Not a valid option.\n");
-                break;
+            case 1: mult_mat(50000, run_times(0), when_record(0)); break;
+            case 2: check_mat(50000, run_times(0), when_record(0)); break;
+            case 3: print_speed(2000, run_times(0), when_record(0)); break;
+            case 4: string(300000, run_times(0), when_record(0)); break;
+            case 5: pi(1000000000, run_times(0), when_record(0)); break;
+            case 6: fib1(150, run_times(0), when_record(0)); break;
+            case 7: division(70000, run_times(0), when_record(0)); break;
+            case 8: doubles(900000000, run_times(0), when_record(0)); break;
+            case 9: file_write(100, run_times(0), when_record(0)); break;
+            case 10: fib4(100000, run_times(0), when_record(0)); break;
+            case 11: queue(); break;
+            case 12: printf("Enter the log number you would like to clear or 0 to clear all: ");
+                     int log; scanf("%d", &log); clear(log); break;
+            case 0: return 0;
+            default: printf("Not a valid option.\n"); break;
         }
         enterToContinue();
     }
